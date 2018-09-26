0ad01bdce9494dc28dda908ecca5e70d6861419b
Vineet Reddy <hotshot2797@gmail.com>
2018-09-27 02:34:09 +0530

    MY GOD. FINALLY. It works!

---
+++ weredoingthisagain.py
@@ -0,0 +1,131 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import random
+import math
+
+# data_X = []
+
+#FORMAT = <whatparameter><whichneuron>
+
+#Opening the file
+fileopen = open('traindata')
+
+# data_X = np.loadtxt(fileopen, usecols=(1))
+# data_Y = np.loadtxt(fileopen, usecols=(0))
+
+data_X = [15,16,17,18,19]
+data_Y = [13,14,15,16,17]
+
+#Parameters for neural network
+hidden = 3
+output = 1
+input = 1
+eta = 0.5
+errors = []
+bias = 1
+
+# W1 = np.random.randn(input, hidden)
+
+W1 = [[random.uniform(-1.0, 1.0) for i in range(input)] for i in range(hidden)]
+W2 = [[random.uniform(-1.0, 1.0) for i in range(hidden)] for i in range(output)]
+# print (W1)
+# print (W2)
+# 
+#Hidden
+h_Hidden = []
+for i in range(0,hidden):
+    h_Hidden.append(0)
+sum_Hidden = [0 for i in range(hidden)] #Will also contain sigmoid output
+delta_Hidden = [0 for i in range(hidden)]
+sumPrime_Hidden = [0 for i in range(hidden)]
+sum_delta_weight = [0 for i in range(hidden)] #That sum thing for delta J
+sigmoidHiddenSi = [0 for i in range(hidden)]
+
+#Output
+yhat_Output = [] #Will contain Sigmoid output
+for i in range(0, output):
+    yhat_Output.append(0)
+
+sum_Output = [0 for i in range(output)]
+delta_Output = [0 for i in range(output)]
+sumPrime_Output = [0 for i in range(output)]
+
+#Sigmoidssss
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoidPrime(x):
+    return x * (1-x)
+
+def forward(X):
+    #Input to Hidden
+    for j in range(0, hidden):
+        h_Hidden[j] = 0
+        for k in range(0, input):
+            h_Hidden[j]= h_Hidden[j]+W1[j][k]*X +bias
+        sum_Hidden[j] = sigmoid(h_Hidden[j])
+
+    #Hidden to output
+    for i in range(0, output):
+        for j in range(0, hidden):
+            sum_Output[i]=0
+            yhat_Output[i] = W2[i][j]*sum_Hidden[j] + bias
+            # print("yhat ",yhat_Output[i])
+            sum_Output[i] += yhat_Output[i]
+        sigmoidHiddenSi[i] = sum_Output[i]
+        # print("S_I", sigmoidHiddenSi)
+        sum_Output[i] = sigmoid(sum_Output[i])
+        # print("Sum_OUTPUT", sum_Output)
+
+
+def backward(X, Y):
+    error = Y - sum_Output[0]
+    for i in range(0, output):
+        sumPrime_Output[i] = sigmoidPrime(sigmoidHiddenSi[i])
+        # print("Sumprime 2" ,sumPrime_Output[i])
+        delta_Output[i] = error*sumPrime_Output[i]
+        # print("Delta Output:  ",delta_Output)
+
+    for j in range(0, hidden):
+        sumPrime_Hidden[j] = sigmoidPrime(h_Hidden[j]) 
+        for i in range(0, output):
+            sum_delta_weight[j] = delta_Output[i]*W2[i][j]
+
+        delta_Hidden[j] = sum_delta_weight[j]*sumPrime_Hidden[j]
+    # print("Delta Hidden ", delta_Hidden)
+
+    #Weight updating
+    for i in range(0, output):
+        for j in range(0, hidden):
+            # print(W2)
+            W2[i][j] = W2[i][j] - eta*delta_Output[i]*sum_Hidden[j]
+            # delta_Output[i]=0
+            # print("Delta", delta_Output)
+            # print("H_HIDDEN", h_Hidden)
+
+    for j in range(0, hidden):
+        for k in range(0, input):
+            W1[j][k] = W1[j][k] - eta*delta_Hidden[j]*X
+            # delta_Hidden[j]=0
+
+    
+    # errSquare = (Y -sum_Output[0]**2)
+    print ("Error", error)
+    # errors.append(errSquare)
+
+
+# data_X = [0.1,0.2,0.3,0.4,0.5]
+# data_Y = [0.2,0.3,0.4,0.5,0.6]
+
+for j in range(0,10): #Number of epcochs
+    for i in range(0,5):
+        forward(data_X[i])
+        backward(data_X[i],data_Y[i])
+        
+        # print(hello)
+    print("End line")
+print (W1)
+print (W2)
+# plt.plot(errors)
+# plt.show()
+# print(sigmoidPrime(10))
--- yaoi.py
+++ yaoi.py
@@ -1,27 +1,43 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import random
-data_X = []
+# data_X = []
 
 #Opening the file
 fileopen = open('traindata')
-data = np.loadtxt(fileopen, usecols=(0))
+
+# data_X = np.loadtxt(fileopen, usecols=(1))
+# data_Y = np.loadtxt(fileopen, usecols=(0))
+
+data_X = [15,16,17,18,19,20,21,22,23,24,25]
+data_Y = [13,14,15,16,17,18,19,20,21,22,23]
 
 #Parameters for neural network
 hidden = 3
 output = 1
 input = 1
-eta = 0.5
+eta = 0.1
+errors = []
 
 #Random weights
 W1 = np.random.randn(input, hidden)
 W2 = np.random.randn(hidden, output)
 sumHidden_J = [[0 for i in range(input)] for j in range(hidden)]
 sumOutput_J = [[0 for i in range(hidden)] for j in range(output)]
-deltaHidden = [[0 for i in range(input)] for j in range(hidden)]
+# deltaHidden = [[0 for i in range(input)] for j in range(hidden)]
+deltaHidden = []
+
+for i in range(0, hidden):
+    deltaHidden.append(0)
+
+
+
 fPrimeOutput = [[0 for i in range(hidden)] for j in range(output)]
 fPrimeHidden = [[0 for i in range(input)] for j in range(hidden)]
 deltaOutput = [[0 for i in range(hidden)] for j in range(output)]
+sumDeltaHidden = []
+for i in range(0, hidden):
+    sumDeltaHidden.append(0)
 # print(sumHidden_J)
 # print(W1)
 error = 0
@@ -46,20 +62,34 @@ def forward(X):
             sumOutput_J[i] = sumOutput_J[i] + W2[j][i]*sumHidden_J[j]
             sumOutput_J[i] = sigmoid(sumOutput_J[i])
 
+
+
 def backward(X, Y):
 
-    error = 0.5*((Y - sumOutput_J[0])**2)
+    error = Y - sumOutput_J[0]
+    # print(sumOutput_J[0])
     print(error)
+    errors.append(error)
     # fPrimeOutput = sigmoidPrime(sumOutput_J) 
     # deltaOutput = error*fPrimeOutput
 
     # fPrimeHidden = sigmoidPrime(sumHidden_J)
     for i in range(0,output):
-        for j in range(0, hidden):
-            fPrimeOutput[i] = sigmoidPrime(sumOutput_J[i])
-            deltaOutput[i] = error*fPrimeOutput[i] 
-            fPrimeHidden[j] = sigmoidPrime(sumHidden_J[j])
-            deltaHidden[j] = deltaOutput[i]*W2[j][i]*fPrimeHidden[j]
+        fPrimeOutput[i] = sigmoidPrime(sumOutput_J[i])
+        deltaOutput[i] = error*fPrimeOutput[i] 
+
+
+    for j in range(0, hidden):
+        fPrimeHidden[j] = sigmoidPrime(sumHidden_J[j])
+        for i in range (0, output):
+            sumDeltaHidden[j] += deltaOutput[i]*W2[j][i]
+        deltaHidden[j]= sumDeltaHidden[j]*fPrimeHidden[j]
+
+
+    # for i in range(0,hidden):
+    #     for j in range(0, input):
+    #         fPrimeHidden[j] = sigmoidPrime(sumHidden_J[j])
+    #         deltaHidden[j] = deltaOutput[i]*W2[j][i]*fPrimeHidden[j]
 
 
     #Weight UPDATIONS
@@ -71,12 +101,20 @@ def backward(X, Y):
     #Input --> Hidden
     for i in range(0,hidden):
         for j in range(0,input):
-            W1[j][i] += eta*deltaHidden[i][j]*X
+            W1[j][i] += eta*deltaHidden[i]*X
+    return sumOutput_J[0]
 
-for i in range(0,1000):
-    forward(2)
-    backward(2,3)
-
+for j in range(0,10):
+    for i in range(0,10):
+        forward(data_X[i])
+        backward(data_X[i],data_Y[i])
+        
+        # print(hello)
+    print("End line")
+plt.plot(errors)
+# print(data_X)
+# print(data_Y)
+plt.show()
 # print(sumHidden_J)
 # print(sumOutput_J)
 # print(deltaHidden)
